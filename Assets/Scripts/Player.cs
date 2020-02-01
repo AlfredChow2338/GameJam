a2fda18c08851934e6aeb910a902ee4baae288fe
alfredchow2338 <alfredchow2338@gmail.com>
2020-02-01 15:00:55 +0800

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // boolean
+    public bool disablePlayer = false;
+
+    public float moveSpeed = 1.0f;
+
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+
+    public KeyCode upKeyCode = KeyCode.W;
+    public KeyCode downKeyCode = KeyCode.S;
+    public KeyCode leftKeyCode = KeyCode.A;
+    public KeyCode rightKeyCode = KeyCode.D;
+
+    public KeyCode actionKeyCode = KeyCode.Space;
+
+    private string TAG_INTERACTABLE = "Interactable";
+
+    private GameObject _activeItem = null;
+
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+    private Vector2 _facingDirection = Vector2.up;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _movement.x = Input.GetAxisRaw(horizontalAxis);
+        _movement.y = Input.GetAxisRaw(verticalAxis);
+
+        if (Input.GetKeyDown(upKeyCode))
+            _facingDirection = Vector2.up;
+        else if (Input.GetKeyDown(downKeyCode))
+            _facingDirection = Vector2.down;
+        else if (Input.GetKeyDown(leftKeyCode))
+            _facingDirection = Vector2.left;
+        else if (Input.GetKeyDown(rightKeyCode))
+            _facingDirection = Vector2.right;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, _facingDirection, 10.0f);
+        Debug.DrawRay(transform.position, _facingDirection);
+
+        if (hit.collider != null && hit.collider.gameObject.CompareTag(TAG_INTERACTABLE) && Input.GetKeyUp(actionKeyCode))
+        {
+            var interactObject = hit.collider.gameObject;
+            interactObject.GetComponent<Interactable>().Interact();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!disablePlayer)
+        {
+            _rb.MovePosition(_rb.position + _movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public static float durationInSecond = 3.0f;
+    public static bool isStart = false;
+    public static bool isStop = true;
+
+    public static float remainingTime {
+        get
+        {
+            return _remainingTime;
+        }
+        set
+        {
+            _remainingTime = value;
+            UpdateTimeUI();
+        }
+    }
+
+    private static float _remainingTime;
+    private static float _endTime = 0.0f;
+    private static TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        UpdateTimeUI();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isStop)
+        {
+            remainingTime = _endTime - Time.time;
+            if (remainingTime <= 1)
+            {
+                isStop = true;
+            }
+        }
+    }
+
+    private static void UpdateTimeUI()
+    {
+        _text.text = remainingTime.CovertToTimeFormatString();
+    }
+
+    [ContextMenu("Start Timer")]
+    public static void StartTimer()
+    {
+        isStart = true;
+        isStop = false;
+        _endTime = Time.time + durationInSecond + 1.0f;
+    }
+
+    [ContextMenu("Stop Timer")]
+    public static void StopTimer()
+    {
+        isStop = true;
+        remainingTime = 0.0f;
+    }
+}
